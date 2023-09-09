b363dfb0784ab453ded2ee69b387e95f8afa6f82
thiagoregueira <thiago.regueira@yahoo.com.br>
2023-09-09 13:45:26 -0300

    criacao das entidades modelos: Comentario, Post e Usuario. Inclusao da dependencia validation no pom.xml

---
+++ blog/src/main/java/com/desafio/blog/model/Post.java
@@ -0,0 +1,44 @@
+package com.desafio.blog.model;
+
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPost;
+
+    @Column(nullable = false, length = 255)
+    private String titulo;
+
+    @Column(nullable = true)
+    private String conteudo;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private Date dataDeCriacao;
+
+    @ManyToOne
+    @JoinColumn(name = "idAutor")
+    private Usuario usuario;
+
+}
---
+++ blog/src/main/java/com/desafio/blog/model/Usuario.java
@@ -0,0 +1,42 @@
+package com.desafio.blog.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+
+    @Column(nullable = false, length = 50)
+    private String nome;
+
+    @Column(nullable = false, length = 150)
+    private String sobrenome;
+
+    @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    @NotEmpty(message = "Email cannot be empty")
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,16}$")
+    @Column(nullable = false, length = 20)
+    private String senha;
+
+}
