c6fd96c801335e72986378cc7461141b6d93e264
thiagoregueira <thiago.regueira@yahoo.com.br>
2023-09-09 20:16:12 -0300

    alteracao do postRepository, metodo findById personalizado.

--- blog/src/main/java/com/desafio/blog/repository/PostRepository.java
+++ blog/src/main/java/com/desafio/blog/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.desafio.blog.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,4 +11,6 @@ import com.desafio.blog.model.Usuario;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findByAutor(Usuario autor);
+
+    Optional<Post> findById(Long idPost);
 }
